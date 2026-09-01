6efa133023a50f1c830798eddaa6c8ea83382ad0
ingrid1322 <ingrid.g1322@gmail.com>
2026-09-01 14:44:04 -0300

    commite

--- src/app/products/products-grid/products-grid.ts
+++ src/app/products/products-grid/products-grid.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 import { ProductCard } from '../product-card/product-card';
+import { signal } from '@angular/core';
+import { Product } from '../product';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
-  imports: [ProductCard],
+  imports: [ProductCard, MatIcon],
   selector: 'app-products-grid',
   styleUrl: './products-grid.scss',
   templateUrl: './products-grid.html',
 })
-export class ProductsGrid {}
+export class ProductsGrid {
+
+  protected readonly products = signal<Product[]>([
+    {
+      id: 1,
+      name: 'Fones de ouvido sem fio premium',
+      description: 'Descrição do produto',
+      price: 199.99,
+      originalPrice: 249.99,
+    },
+    {
+      id: 2,
+      name: 'Relógio inteligente fitness',
+      description: 'Descrição do produto',
+      price: 299.99
+    },
+    {
+      id: 3,
+      name: 'Caixa de som Bluetooth',
+      description: 'Descrição do produto',
+      price: 79.99,
+    }
+  ]);
+}
